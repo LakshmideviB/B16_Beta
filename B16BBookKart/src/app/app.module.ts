19a4170a3cefbcdf5d3aef03f4ed6f258792d9b8
LakshmideviB <lakshmidevi.b2407@gmail.com>
2021-11-28 17:21:58 +0530

    Created Responsive Title Bar

--- B16BBookKart/src/app/app.module.ts
+++ B16BBookKart/src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
+import { NgMaterialModule } from './ng-material/ng-material.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SearchComponent } from './search/search.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import {
+  StoreModule
+} from '@ngrx/store';
+import { counterReducer } from './store/reducers/counter';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavBarComponent, SearchComponent, WishlistComponent, ShoppingCartComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    NgMaterialModule,
+    StoreModule.forRoot({ 
+      count: counterReducer
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
